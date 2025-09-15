fa39539555128cb313db1f9adefe588dab2a1f22
Larp-Thon <retrox8yt@gmail.com>
2025-09-14 23:39:06 -0500

    Cambios finales a los endpoints

--- infrastructure/driven-adapters/jpa-repository/src/main/java/co/edu/ucompensar/jpa/entity/ProductionCompanyEntity.java
+++ infrastructure/driven-adapters/jpa-repository/src/main/java/co/edu/ucompensar/jpa/entity/ProductionCompanyEntity.java
@@ -1,9 +1,6 @@
 package co.edu.ucompensar.jpa.entity;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.*;
 
 @Entity
@@ -15,6 +12,7 @@ import lombok.*;
 @Builder
 public class ProductionCompanyEntity {
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     @Column
     private String name;
